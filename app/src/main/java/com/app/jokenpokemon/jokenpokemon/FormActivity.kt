04097307d1b7cf9f506d4ed0b2641628b5af302d
danielideriba@gmail.com <danielideriba@gmail.com>
2018-12-04 20:46:26 -0200

    login

--- app/src/main/java/com/app/jokenpokemon/jokenpokemon/FormActivity.kt
+++ app/src/main/java/com/app/jokenpokemon/jokenpokemon/FormActivity.kt
@@ -7,6 +7,20 @@ class FormActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form)
+        setContentView(R.layout.activity_main)
+
+//        etGasPrice.addTextChangedListener(DecimalTextWatcher(etGasPrice))
+//        etEthanolPrice.addTextChangedListener(DecimalTextWatcher(etEthanolPrice))
+//
+//        etGasAverage.addTextChangedListener(DecimalTextWatcher(etGasAverage))
+//
+//        btCalculate.setOnClickListener({
+//            val intent = Intent(this, ResultActivity::class.java)
+//            intent.putExtra("GAS_PRICE", etGasPrice.text.toString().toDouble())
+//            intent.putExtra("ETHANOL_PRICE", etEthanolPrice.text.toString().toDouble())
+//            intent.putExtra("GAS_AVERAGE", etGasAverage.text.toString().toDouble())
+//            intent.putExtra("ETHANOL_AVERAGE", etEthanolAverage.text.toString().toDouble())
+//            startActivity(intent)
+//        })
     }
 }
