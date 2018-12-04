959add11aa6566772bf84a973facf62c770dfa1c
Logon Aluno <logonrmlocal@fiap.com.br>
2018-12-04 19:55:41 -0200

    Initial commit

---
+++ app/src/main/java/com/app/jokenpokemon/jokenpokemon/SplashActivity.kt
@@ -0,0 +1,53 @@
+package com.app.jokenpokemon.jokenpokemon
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.animation.AnimationUtils
+
+class SplashActivity : AppCompatActivity() {
+
+    private val TEMPO_AGUARDO_SPLASHSCREEN = 3500L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val preferences = getSharedPreferences("user_preferences", Context.MODE_PRIVATE)
+        val isFirstOpen = preferences.getBoolean("open_first", true)
+
+        if (isFirstOpen) {
+            showLogin()
+        } else {
+            markAppAlreadyOpen(preferences)
+            showSplash()
+        }
+    }
+
+    private fun markAppAlreadyOpen(preferences: SharedPreferences) {
+        val editor = preferences.edit()
+        editor.putBoolean("open_first", false)
+        editor.apply()
+    }
+
+    private fun showLogin() {
+        Log.i("TAG", "LOGIN")
+//        val nextScreen = Intent(this@SplashActivity, LoginActivity::class.java)
+//        startActivity(nextScreen)
+//        finish()
+    }
+
+    private fun showSplash() {
+        val anim = AnimationUtils.loadAnimation(this, R.anim.anim_splash)
+        anim.reset()
+//        ivLogo.clearAnimation()
+//        ivLogo.startAnimation(anim)
+
+        Handler().postDelayed({
+            showLogin()
+        }, TEMPO_AGUARDO_SPLASHSCREEN)
+    }
+}
