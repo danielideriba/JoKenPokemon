1f7999f8b3c5b0f6d00ea84efddc2641ed36398e
danielideriba@gmail.com <danielideriba@gmail.com>
2018-12-04 21:53:17 -0200

    id tela de game

--- app/src/main/java/com/app/jokenpokemon/jokenpokemon/GameScreenActivity.kt
+++ app/src/main/java/com/app/jokenpokemon/jokenpokemon/GameScreenActivity.kt
@@ -2,11 +2,29 @@ package com.app.jokenpokemon.jokenpokemon
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_game_screen.*
 
 class GameScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_screen)
+
+
+        charizard.setOnClickListener({
+            Log.i("TAG", "charizard")
+        })
+
+        venusaur.setOnClickListener({
+            Log.i("TAG", "venu")
+        })
+
+        blastoise.setOnClickListener({
+            Log.i("TAG", "blastoise")
+        })
+
+
+
     }
 }
