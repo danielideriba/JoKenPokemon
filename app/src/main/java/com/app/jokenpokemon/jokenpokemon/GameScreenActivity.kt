40145752caa49e0dae36eaee8d2343ac1b2ecdd6
danielideriba@gmail.com <danielideriba@gmail.com>
2018-12-04 22:59:09 -0200

    logica

--- app/src/main/java/com/app/jokenpokemon/jokenpokemon/GameScreenActivity.kt
+++ app/src/main/java/com/app/jokenpokemon/jokenpokemon/GameScreenActivity.kt
@@ -1,30 +1,60 @@
 package com.app.jokenpokemon.jokenpokemon
 
+import android.annotation.TargetApi
+import android.content.Context
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_game_screen.*
+import java.util.*
 
 class GameScreenActivity : AppCompatActivity() {
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_screen)
 
+        startGame(this)
 
         charizard.setOnClickListener({
-            Log.i("TAG", "charizard")
+            player1Container.setImageDrawable(getDrawable(R.drawable.img_fire))
+            player1Container.setTag("img_fire")
+            startGame(this)
+            calcGame(player1Container.tag, cpuContainer.tag)
         })
 
         venusaur.setOnClickListener({
-            Log.i("TAG", "venu")
+            player1Container.setImageDrawable(getDrawable(R.drawable.img_leaf))
+            player1Container.setTag("img_leaf")
+            startGame(this)
+            calcGame(player1Container.tag, cpuContainer.tag)
         })
 
         blastoise.setOnClickListener({
-            Log.i("TAG", "blastoise")
+            player1Container.setImageDrawable(getDrawable(R.drawable.img_water))
+            player1Container.setTag("img_water")
+            startGame(this)
+            calcGame(player1Container.tag, cpuContainer.tag)
         })
+    }
 
+    private fun calcGame(tagPlayer: Any, tagCPU: Any) {
+        Log.i("TAG", "--player-"+tagPlayer)
+        Log.i("TAG", "--CPU-"+tagCPU)
 
+        if(tagPlayer == "img_leaf" && tagCPU == "img_fire"){
+        }
 
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun startGame(context: Context){
+        val elements = resources.getStringArray(R.array.elements_game)
+        val randomStr = elements[Random().nextInt(elements.size)]
+        val resID = resources.getIdentifier(randomStr, "drawable", packageName)
+        cpuContainer.setImageDrawable(context.getDrawable(resID))
+        cpuContainer.setTag(randomStr)
     }
 }
